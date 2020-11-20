c654a3aa11d6e13b71084096ef01ea1ed80c66a8
Maximiliano <maxiolaz@gmail.com>
2020-11-20 00:55:36 -0300

    Vista de publicaciones en el template

--- AerApp/views.py
+++ AerApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from publicaciones.models import Publicacion
 
 # Create your views here.
 
@@ -12,7 +13,8 @@ def equipo(request):
     return render(request, "AerApp/equipo.html")
 
 def publicaciones(request):
-    return render(request, "AerApp/publicaciones.html")
+    publicaciones=Publicacion.objects.all()
+    return render(request, "AerApp/publicaciones.html", {"publicaciones": publicaciones})
 
 def contacto(request):
     return render(request, "AerApp/contacto.html")
